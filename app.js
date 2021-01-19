18e8d4aa0c4eed2a846690115582af3dc8d9db2f
Bold <sanchirsanchir976@gmail.com>
2021-01-19 16:48:29 +0800

    key eventlistener bichih

---
+++ app.js
@@ -0,0 +1,23 @@
+// Дэлгэцтэй харицах controllor
+var uiController = (function () {})();
+
+var financeController = (function () {})();
+var appController = (function (uiController, financeController) {
+  var ctrlAddItem = function () {
+    // 1. Оруулах өгөгдлийг дэлгэцээс авах.
+    console.log("Дэлгэцээс өгөгдлөө авах хэсэг");
+    // 2. Олж авсан өгөгдлүүдээ санхүүгийн контроллерт дамжуулж тэнд хадгална.
+    // 3. Олж авсан өгөгдлөө вэб дээрээ тохирох хэсэгт гаргана
+    // 4. Төсвийг тооцоолно.
+    // 5. Эцсийн үлдэгдэл тооцоог дэлгэцэнд хэвлэх.
+  };
+  document.querySelector(".add__btn").addEventListener("click", function () {
+    ctrlAddItem();
+  });
+
+  document.addEventListener("keypress", function (event) {
+    if (event.keyCode === 13 || event.which === 13) {
+      ctrlAddItem();
+    }
+  });
+})(uiController, financeController);
